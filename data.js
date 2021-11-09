7686e0dd99e4226afc1e057e2e70cbc7d074fa68
shanew-NAIT <93292360+shanew-NAIT@users.noreply.github.com>
2021-11-09 13:59:16 -0700

    Nov 9

--- data.js
+++ data.js
@@ -24,19 +24,38 @@ var APP_DATA = {
       ],
       "faceSize": 1344,
       "initialViewParameters": {
-        "yaw": -0.16033489956936364,
-        "pitch": -0.0068354979829283735,
-        "fov": 1.4019787994768
+        "yaw": 0.6390590184225609,
+        "pitch": 0.03994269243199611,
+        "fov": 1.2933824216075565
       },
       "linkHotspots": [
         {
-          "yaw": 1.6058588758839383,
-          "pitch": 0.11913066958566532,
+          "yaw": 1.6293783777438904,
+          "pitch": 0.11781387734806259,
           "rotation": 0,
           "target": "1-makerspace2"
+        },
+        {
+          "yaw": -0.3436889176977793,
+          "pitch": 0.18854296948883942,
+          "rotation": 0,
+          "target": "3-makerspace4"
         }
       ],
-      "infoHotspots": []
+      "infoHotspots": [
+        {
+          "yaw": -1.4063360198075365,
+          "pitch": 0.20962400077532806,
+          "title": "Ultimaker 3D printers",
+          "text": "3D Print stuff!"
+        },
+        {
+          "yaw": 0.14814257424441024,
+          "pitch": 0.17743633166484152,
+          "title": "Display cabinet",
+          "text": "Text"
+        }
+      ]
     },
     {
       "id": "1-makerspace2",
@@ -62,25 +81,32 @@ var APP_DATA = {
       ],
       "faceSize": 1344,
       "initialViewParameters": {
-        "yaw": 0.13380283056162057,
-        "pitch": 0.07114519280927212,
-        "fov": 1.4019787994768
+        "yaw": -0.1167909404108034,
+        "pitch": 0.058946215078691466,
+        "fov": 1.2933824216075565
       },
       "linkHotspots": [
         {
-          "yaw": -3.0928054379614025,
-          "pitch": 0.13307169801368168,
+          "yaw": 0.10804004558897873,
+          "pitch": 0.20980966501629084,
           "rotation": 0,
-          "target": "0-makerspace1"
+          "target": "2-makerspace3"
         },
         {
-          "yaw": 0.12644841476215518,
-          "pitch": 0.1287393533366732,
-          "rotation": 6.283185307179586,
-          "target": "2-makerspace3"
+          "yaw": -3.0665332410470008,
+          "pitch": 0.1835714567777913,
+          "rotation": 0,
+          "target": "0-makerspace1"
         }
       ],
-      "infoHotspots": []
+      "infoHotspots": [
+        {
+          "yaw": -0.3265059178302323,
+          "pitch": 0.21798574283898553,
+          "title": "X-Carve",
+          "text": "Create stuff with CNC"
+        }
+      ]
     },
     {
       "id": "2-makerspace3",
@@ -112,20 +138,78 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 3.1054273000516543,
-          "pitch": 0.25563793267988366,
+          "yaw": -3.130822266610757,
+          "pitch": 0.22718083188973281,
           "rotation": 0,
           "target": "1-makerspace2"
         }
       ],
-      "infoHotspots": []
+      "infoHotspots": [
+        {
+          "yaw": 2.1755590875206616,
+          "pitch": 0.2792300760970772,
+          "title": "Laser Engraver<div><br></div>",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.17275048621570122,
+          "pitch": 0.0971987407239574,
+          "title": "Vinyl Cutter",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "3-makerspace4",
+      "name": "makerspace4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5632847166301005,
+          "pitch": 0.2853948609206114,
+          "rotation": 0,
+          "target": "0-makerspace1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.359566732663593,
+          "pitch": -0.008291936010252243,
+          "title": "We also have books!",
+          "text": "Text"
+        }
+      ]
     }
   ],
-  "name": "NAIT Library Makerspace",
+  "name": "Project Title",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": false,
-    "fullscreenButton": true,
-    "viewControlButtons": true
+    "fullscreenButton": false,
+    "viewControlButtons": false
   }
 };
